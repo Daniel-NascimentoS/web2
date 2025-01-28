a68df7b97d22db4e1a4861f6fcde922c773bb1de
Daniel Nascimento <danielnascimento.1712@gmail.com>
2025-01-28 16:40:24 -0300

    Alterações para publicação em algum serviço online

--- microblog.py
+++ microblog.py
@@ -2,3 +2,8 @@
 utilizar o seguinte comando para rodar "flask --app microblog run --debug"
 '''
 from app import app
+import os
+
+if __name__ == 'main':
+    port = int(os.getenv('PORT'), '5000')
+    app.run(host='0.0.0.0', port = port)
